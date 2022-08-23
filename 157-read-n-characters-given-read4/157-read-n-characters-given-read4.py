b0b38ab217cc0a5d9210e6710837586584faee81
JeongminCha <cjm9236@gmail.com>
2022-08-23 20:17:47 +0900

    Time: 63 ms (11.94%), Space: 13.8 MB (86.15%) - LeetHub

---
+++ 157-read-n-characters-given-read4/157-read-n-characters-given-read4.py
@@ -0,0 +1,30 @@
+"""
+The read4 API is already defined for you.
+
+    @param buf4, a list of characters
+    @return an integer
+    def read4(buf4):
+
+# Below is an example of how the read4 API can be called.
+file = File("abcdefghijk") # File is "abcdefghijk", initially file pointer (fp) points to 'a'
+buf4 = [' '] * 4 # Create buffer with enough space to store characters
+read4(buf4) # read4 returns 4. Now buf = ['a','b','c','d'], fp points to 'e'
+read4(buf4) # read4 returns 4. Now buf = ['e','f','g','h'], fp points to 'i'
+read4(buf4) # read4 returns 3. Now buf = ['i','j','k',...], fp points to end of file
+"""
+
+class Solution:
+    def read(self, buf, n):
+        index = 0
+        while index < n:
+            buf4 = [" "] * 4
+            count = read4(buf4)
+            if count == 0:
+                break
+            
+            count = min(count, n - index)
+            buf[index:] = buf4[:count]
+            index += count
+        
+        return index
+        
