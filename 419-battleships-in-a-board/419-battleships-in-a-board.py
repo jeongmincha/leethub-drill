652e79c51a0fa7806496651d12077c0685ab35e8
JeongminCha <cjm9236@gmail.com>
2022-08-28 21:50:58 +0900

    Time: 109 ms (59.19%), Space: 14.6 MB (21.28%) - LeetHub

--- 419-battleships-in-a-board/419-battleships-in-a-board.py
+++ 419-battleships-in-a-board/419-battleships-in-a-board.py
@@ -3,33 +3,24 @@ class Solution:
         DIRECTIONS = [(1,0), (0,1)]
         M = len(board)
         N = len(board[0])
-        visited = [[False for _ in range(N)] for _ in range(M)]
         
-        for x in range(M):
-            for y in range(N):
-                if board[x][y] == '.':
-                    visited[x][y] = True
-        
-        def dfs(pos, direction): # pos = (x, y), direction = (dx, dy) # (1,2), (-1,0)
-            nonlocal visited
-            if pos[0] < 0 or pos[0] >= M or pos[1] < 0 or pos[1] >= N or visited[pos[0]][pos[1]]:
+        def dfs(pos):
+            nonlocal board
+            if pos[0] < 0 or pos[0] >= M or pos[1] < 0 or pos[1] >= N or board[pos[0]][pos[1]] == '.':
                 return
             
-            visited[pos[0]][pos[1]] = True
-            nx, ny = pos[0] + direction[0], pos[1] + direction[1]
-            dfs((nx, ny), direction)
+            board[pos[0]][pos[1]] = '.'
+            for direction in DIRECTIONS:
+                nx, ny = pos[0] + direction[0], pos[1] + direction[1]
+                dfs((nx, ny))
         
         answer = 0
         for x in range(M):
             for y in range(N):
-                if visited[x][y]:
+                if board[x][y] == '.':
                     continue
                 
-                for direction in DIRECTIONS:
-                    visited[x][y] = False
-                    dfs((x,y), direction)
-                
-                visited[x][y] = True
+                dfs((x,y))
                 answer += 1
 
         return answer
