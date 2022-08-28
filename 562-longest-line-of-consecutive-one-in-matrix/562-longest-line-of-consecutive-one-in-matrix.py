1d5cd5b34699242b1b67881745389dcdb6b7c3fe
JeongminCha <cjm9236@gmail.com>
2022-08-29 02:18:23 +0900

    Time: 981 ms (25.24%), Space: 17.3 MB (73.51%) - LeetHub

--- 562-longest-line-of-consecutive-one-in-matrix/562-longest-line-of-consecutive-one-in-matrix.py
+++ 562-longest-line-of-consecutive-one-in-matrix/562-longest-line-of-consecutive-one-in-matrix.py
@@ -3,8 +3,6 @@ class Solution:
         M = len(mat)
         N = len(mat[0])
         
-        # O(MN^2 + M^2N) -> O(M * N) [DP]
-        
         dp = [[[0 for _ in range(4)] for _ in range(N)] for _ in range(M)] # M * N * 4
         # dp[x][y][0] = the length of the line until (x,y) in horizontal line (to the right)
         # dp[x][y][1] = the length of the line until (x,y) in vertical line (to the bottom)
@@ -14,21 +12,23 @@ class Solution:
         answer = 0
         for r in range(M):
             for c in range(N):
-                if mat[r][c] == 1:
-                    dp[r][c][0] = dp[r][c][1] = dp[r][c][2] = dp[r][c][3] = mat[r][c]
-                    if r > 0:
-                        dp[r][c][0] = dp[r-1][c][0] + 1
-                    if c > 0:
-                        dp[r][c][1] = dp[r][c-1][1] + 1
-                    if r > 0 and c > 0:
-                        dp[r][c][2] = dp[r-1][c-1][2] + 1
-                    if r > 0 and c < N-1:
-                        dp[r][c][3] = dp[r-1][c+1][3] + 1
-                    
-                    answer = max(answer, dp[r][c][0])
-                    answer = max(answer, dp[r][c][1])
-                    answer = max(answer, dp[r][c][2])
-                    answer = max(answer, dp[r][c][3])
+                if mat[r][c] == 0:
+                    continue
+                
+                dp[r][c][0] = dp[r][c][1] = dp[r][c][2] = dp[r][c][3] = mat[r][c]
+                if r > 0:
+                    dp[r][c][0] = dp[r-1][c][0] + 1
+                if c > 0:
+                    dp[r][c][1] = dp[r][c-1][1] + 1
+                if r > 0 and c > 0:
+                    dp[r][c][2] = dp[r-1][c-1][2] + 1
+                if r > 0 and c < N-1:
+                    dp[r][c][3] = dp[r-1][c+1][3] + 1
+
+                answer = max(answer, dp[r][c][0])
+                answer = max(answer, dp[r][c][1])
+                answer = max(answer, dp[r][c][2])
+                answer = max(answer, dp[r][c][3])
         
         return answer
                 
