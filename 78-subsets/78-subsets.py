03f6804ca15d04ee3406b55282c46444e3fcb104
JeongminCha <cjm9236@gmail.com>
2022-09-03 16:31:51 +0900

    Time: 58 ms (38.01%), Space: 14.2 MB (35.77%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -4,33 +4,36 @@ class Solution:
         # 2: {},{2}, {1},{1,2}
         # 3: {},{3}, {2},{2,3}, {1}, {1,3}, {1,2}, {1,2,3} => 8 cases
         
-        N = len(nums)
-        answer = []
+#         # 1. Recursive way
+#         N = len(nums)
+#         answer = []
         
-        def helper(idx, subset):
-            nonlocal answer
-            if idx == N:
-                answer.append([e for e in subset if e is not None])
-            else:
-                subset[idx] = None
-                helper(idx+1, subset)
-                subset[idx] = nums[idx]
-                helper(idx+1, subset)
+#         def helper(idx, subset):
+#             nonlocal answer
+#             if idx == N:
+#                 answer.append([e for e in subset if e is not None])
+#             else:
+#                 subset[idx] = None
+#                 helper(idx+1, subset)
+#                 subset[idx] = nums[idx]
+#                 helper(idx+1, subset)
 
-        subset = [0] * N
-        helper(0, subset)
-        
-        return answer
-        
-#         answer = [[], [nums[0]]]
-        
-#         for i in range(1, N):
-#             num = nums[i]
-#             previous_answer = answer[:] # copy without reference
-#             for e in previous_answer:
-#                 answer.append(e + [])
-#                 answer.append(e + [num])
-            
-#             answer = answer[2 ** i:] # remove the previous duplicate answers
+#         subset = [0] * N
+#         helper(0, subset)
         
 #         return answer
+ 
+        # 2. Iterative way
+        N = len(nums)
+        answer = [[], [nums[0]]]
+        
+        for i in range(1, N):
+            num = nums[i]
+            previous_answer = answer[:] # copy without reference
+            for e in previous_answer:
+                answer.append(e + [])
+                answer.append(e + [num])
+            
+            answer = answer[2 ** i:] # remove the previous duplicate answers
+        
+        return answer
