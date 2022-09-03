f5c9ab0acb7c31518119d8929ee796a7a3463b5d
JeongminCha <cjm9236@gmail.com>
2022-09-03 16:30:24 +0900

    Time: 74 ms (8.50%), Space: 14 MB (82.25%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -5,15 +5,32 @@ class Solution:
         # 3: {},{3}, {2},{2,3}, {1}, {1,3}, {1,2}, {1,2,3} => 8 cases
         
         N = len(nums)
-        answer = [[], [nums[0]]]
+        answer = []
         
-        for i in range(1, N):
-            num = nums[i]
-            previous_answer = answer[:] # copy without reference
-            for e in previous_answer:
-                answer.append(e + [])
-                answer.append(e + [num])
-            
-            answer = answer[2 ** i:] # remove the previous duplicate answers
+        def helper(idx, subset):
+            nonlocal answer
+            if idx == N:
+                answer.append([e for e in subset if e is not None])
+            else:
+                helper(idx+1, subset)
+                subset.append(nums[idx])
+                helper(idx+1, subset)
+                subset.remove(nums[idx])
+
+        subset = []
+        helper(0, subset)
         
         return answer
+        
+#         answer = [[], [nums[0]]]
+        
+#         for i in range(1, N):
+#             num = nums[i]
+#             previous_answer = answer[:] # copy without reference
+#             for e in previous_answer:
+#                 answer.append(e + [])
+#                 answer.append(e + [num])
+            
+#             answer = answer[2 ** i:] # remove the previous duplicate answers
+        
+#         return answer
