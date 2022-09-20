e2fef7c5cc38cccd6edef157d862b8ef78d522ea
JeongminCha <cjm9236@gmail.com>
2022-09-20 23:48:00 +0900

    Time: 754 ms (38.01%), Space: 16 MB (42.06%) - LeetHub

--- 2013-detect-squares/2013-detect-squares.py
+++ 2013-detect-squares/2013-detect-squares.py
@@ -15,24 +15,12 @@ class DetectSquares:
         res = 0
         x, y = point
         
-        if len(self.points) < 1000:
-            for (i, j), cnt in self.points.items():
-                if i == x or j == y or abs(x-i) != abs(y-j):
-                    continue
-                res += cnt * self.points[i, y] * self.points[x, j]
-            return res
-        
-#         for i in range(0, 1001):
-#             if i == x:
-#                 continue
-            
-#             d = abs(x-i)
-#             for j in [y+d, y-d]:
-#                 res += self.points[i, j] * self.points[x, j] * self.points[i, y]
-        
-        return res
-        
+        for (i, j), cnt in self.points.items():
+            if i == x or j == y or abs(x-i) != abs(y-j):
+                continue
+            res += cnt * self.points[i, y] * self.points[x, j]
 
+        return res
 
 # Your DetectSquares object will be instantiated and called as such:
 # obj = DetectSquares()
